b08f2ca2399264e3d57733591722ead80275f451
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-07-15 00:37:59 +0800

    add simple CDC flow in this project

---
+++ cdc_event_handler.go
@@ -0,0 +1,19 @@
+package tubing_cdc
+
+import (
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/siddontang/go-log/log"
+)
+
+type MyEventHandler struct {
+	canal.DummyEventHandler
+}
+
+func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
+	log.Infof("%s %v\n", e.Action, e.Rows)
+	return nil
+}
+
+func (h *MyEventHandler) String() string {
+	return "MyEventHandler"
+}
